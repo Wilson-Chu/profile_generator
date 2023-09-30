f6c9a229573a56fb57abb67efefbf4aff3ab8ec2
Wilson Chu <wilson1.chu@gmail.com>
2023-09-30 00:40:45 -0400

    completed fun profile generator exercise for LHL

--- survey.js
+++ survey.js
@@ -5,8 +5,16 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question('What\'s your name or nickname? ', (answer) => {
+  rl.question('What is your favourite programming language? ', (language) => {
+    rl.question('What do you like to listen to while coding? ', (music) => {
+      rl.question('What do you eat when you\'re not progamming? ', (food) => {
+        rl.question('Amy I asking you too many questions? ', (questions) => {
 
-  rl.close();
+          console.log(`Hi there, I'm ${answer}! I love listening to ${music} while programming in ${language}. When I'm not programming, you can find me eating ${food} and saying "${questions}" to questions you ask me.`);
+          rl.close();
+        });
+      });
+    });
+  });
 });
